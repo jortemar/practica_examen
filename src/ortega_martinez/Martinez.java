88cda65d04177990c0a703750ba72ec01686f660
jortemar <jortemar@gmail.com>
2022-06-06 16:12:31 +0200

    Primera versión. Clases vacías

---
+++ src/ortega_martinez/Martinez.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ortega_martinez;
+
+/**
+ *
+ * @author jorte
+ */
+public class Martinez {
+    
+
+    public static int resta (int num1, int num2){
+        
+        return num1 - num2;
+    }
+    
+}
---
+++ src/ortega_martinez/Ortega_Martinez.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ortega_martinez;
+
+/**
+ *
+ * @author jorte
+ */
+public class Ortega_Martinez {
+
+    public static void main(String[] args) {
+        
+        int resultado_suma;
+        int resultado_resta;
+        
+        resultado_suma = Ortega.suma(2,5);
+        resultado_resta = Martinez.resta(9,6);
+        
+        System.out.println("La suma da " + resultado_suma);
+        System.out.println("La resta da " + resultado_resta);
+    }
+    
+}
